fc54d87e45d877e38bbf25a892bf274f97be3fc2
mogoo7zn <znwangking@hotmail.com>
2023-12-15 15:18:26 +0800

    commit A17

--- A/A17.c
+++ A/A17.c
@@ -1,6 +1,19 @@
 //
 // Created by znwan on 2023/11/15.
 //
+//A17 分析一手5张牌的牌型，输出最好的牌型。牌有四种花色（Diamonds、Clubs、Hearts、Spades）和十三个等级（2、3、4、5、6、7、8、9、10、J、Q、K、A），
+// A既可作为最大等级，也可作为最小等级。输入时，花色用首字母表示，等级用数字或字母表示（10用T表示）。若输入非法牌或重复牌则丢弃。若输入为0则结束程序。
+//   牌型从大到小有：
+//同花顺 straight flush（同花色且等级顺序相连）
+//四张 four of a kind（4张牌等级相同）
+//葫芦 full house（3张牌等级相同，另2张牌也等级相同）
+//同花 flush（5张牌花色相同）
+//顺子 straight（5张牌等级顺序相连）
+//三张 three of a kind（ 3张牌等级相同）
+//两对 two pairs（存在两个对子）
+//对子 pair（2张牌等级相同）
+//其他
+
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,11 +21,14 @@
 #define T 10
 #define LENGTH 15
 #define MAX_LENGTH 1500
+#define TypeLen 4
+#define NumLen 10
 //#define DIAMONDS D
 //#define CLUBS C
 //#define HEARTS H
 //#define SPADES S
 
+int islegal(char *cursor);
 int straight_flush(char *input_cards);
 int four_of_a_kind(char *input_cards);
 int full_house(char *input_cards);
@@ -25,32 +41,63 @@ void bubbleSort(char *value, int left, int right);
 void swap(char *a, char *b);
 
 
-char input_cards_all[MAX_LENGTH];
+char buffer_all[MAX_LENGTH] = "D2 24 HA SJ CK SJ 0";
+char judge_type[4] = {'D', 'C', 'H', 'S'};
+char judge_num[10] = {'A', '2', '3', '4', '5', '6', '7', '8', '9', 'T'};
+char type[5];
+char num[5];
 int main(void)
 {
-    int i = 100;
-    gets(input_cards_all);
-    char *array_ptr = input_cards_all;
-    while(i--) {
-        char input_cards[LENGTH];
-        int cnt;
-        for(cnt = 0; cnt < LENGTH; cnt++){
-            input_cards[cnt] = *array_ptr;
-            array_ptr++;
-        }
-
-        //store input card types
-//        gets_s(input_cards, 15);
-        if (*input_cards == '0') {
-            return 1;
-        }else if (straight_flush(input_cards)) {
-            printf("straight flush\n");
-            continue;
+    char *cursor = buffer_all, *current = buffer_all;
+    int i = 0;
+    while(islegal(cursor) != -1){
+        while(1){
+            if (i == 4)
+                break;
+            if (islegal(cursor)){
+                type[i] = *cursor;
+                num[i] = *(cursor + 1);
+                i++;
+            }
+            cursor += 3;
         }
     }
-//    printf("%s", input_cards);
+
+    return 0;
 }
 
+int islegal(char *cursor)
+{
+    if (*cursor == '0'){
+        return -1;
+    }
+    int i = 0, flag1 = 0, flag2 = 0;
+    while(i < TypeLen){
+        i++;
+        if (*cursor == judge_type[i]) {
+            flag1 = 1;
+            break;
+        }
+    }
+    i = 0;
+    while(i < NumLen){
+        i++;
+        if (*(cursor + 1) == judge_num[i]) {
+            flag2 = 1;
+            break;
+        }
+    }
+    i = 0;
+    if (!(flag1 && flag2)){
+        return 0;
+    }
+    while (i < 5){
+        i++;
+        if (*cursor == type[i] && *(cursor + 1) == num[i])
+            return 0;
+    }
+    return 1;
+}
 //initialize input_cards in incremental sequence
 void init_cards(char *input_cards, char *ch, char *number)
 {
@@ -154,6 +201,29 @@ void bubbleSort(char *value, int left, int right)
     }
 }
 
+
+
+
+//store input card types
+//        gets_s(input_cards, 15);
+//        if (*input_cards == '0') {
+//            return 1;
+//        }else if (straight_flush(input_cards)) {
+//            printf("straight flush\n");
+//            continue;
+//        }
+//    }
+//    printf("%s", input_cards);
+//int i = 100;
+//    gets(input_cards_all);
+//    char *array_ptr = input_cards_all;
+//    while(i--) {
+//        char input_cards[LENGTH];
+//        int cnt;
+//        for(cnt = 0; cnt < LENGTH; cnt++){
+//            input_cards[cnt] = *array_ptr;
+//            array_ptr++;
+//        }
 //think: use a struct or two arrays to store the information?
 
 //    typedef struct card{
